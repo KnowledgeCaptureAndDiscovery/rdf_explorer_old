18bfaca5498c98baeba81fb66d3464c00a67d960
Maximiliano Osorio <maxiosorio@gmail.com>
2019-06-20 17:04:15 -0400

    Fix: construc with graph
    Fix: #12

--- controllers/data.js
+++ controllers/data.js
@@ -1,6 +1,10 @@
 const config  = require('config');
 const request = require('request')
 
+function replaceURI(uri, serverURL, base){
+    return uri.replace(serverURL,base);
+}
+
 exports.data_show = function(req, res) {
     namespace = req.url.split("/")[2];
     //obtain the params according the namespace
@@ -22,10 +26,13 @@ exports.data_show = function(req, res) {
         res.send('Not found');
     }
     else {
-        url = serverURL + path;
-        url = replaceURI(url, serverURL)
-        resFormat = req.accepts(['text/turtle', 'application/ld+json', 'application/rdf+xml', 'application/n-triples'])
-        var q = 'DESCRIBE <' + url + '>';
+        url = baseURI + path;
+        resFormat = req.accepts(['text/turtle', 'application/ld+json', 'application/rdf+xml', 'application/n-triples'])        
+        var q = 'CONSTRUCT { ?s ?o ?p } WHERE {\n';
+        q+= 'GRAPH <' + url + '>';
+        q+= ' { ?s ?o ?p } }';
+                
+        console.log(q);
         request.post(
             queryEndpoint, 
             { 
